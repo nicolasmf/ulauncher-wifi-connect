6cd7763f39d61ebc6e2477404639efcd88c9c9e7
Nicolas MF <nikolamf@hotmail.com>
2022-05-30 21:23:34 +0200

    Connection to know wifi possible

--- main.py
+++ main.py
@@ -1,17 +1,18 @@
-import subprocess
-from black import out
+import subprocess, os
 from ulauncher.api.client.Extension import Extension
 from ulauncher.api.client.EventListener import EventListener
-from ulauncher.api.shared.event import KeywordQueryEvent
+from ulauncher.api.shared.event import KeywordQueryEvent, ItemEnterEvent
 from ulauncher.api.shared.item.ExtensionResultItem import ExtensionResultItem
 from ulauncher.api.shared.action.RenderResultListAction import RenderResultListAction
 from ulauncher.api.shared.action.HideWindowAction import HideWindowAction
+from ulauncher.api.shared.action.ExtensionCustomAction import ExtensionCustomAction
 
 
 class WifiPicker(Extension):
     def __init__(self):
         super().__init__()
         self.subscribe(KeywordQueryEvent, KeywordQueryEventListener())
+        self.subscribe(ItemEnterEvent, ItemEnterEventListener())
 
 
 class KeywordQueryEventListener(EventListener):
@@ -26,16 +27,18 @@ class KeywordQueryEventListener(EventListener):
                 stdout=subprocess.PIPE,
             )
 
-            ssids = set(str(result.stdout).split("\\n")[1:-1])
+            ssids = list(set(str(result.stdout).split("\\n")[1:-1]))
 
             for i in range(len(ssids)):
                 output.append(
                     ExtensionResultItem(
-                        icon="images/error.png",
+                        icon="images/icon.png",
                         name=ssids[i],
-                        description="nmcli package not found.",
-                        on_enter=HideWindowAction(),
-                    )
+                        description="Connect",
+                        on_enter=ExtensionCustomAction(
+                            data={ssids[i]}, keep_app_open=True
+                        ),
+                    ),
                 )
 
             return RenderResultListAction(output)
@@ -50,6 +53,26 @@ class KeywordQueryEventListener(EventListener):
             )
 
             return RenderResultListAction(package_not_found_error)
+
+    def connect():
+        pass
+
+
+class ItemEnterEventListener(EventListener):
+    def on_event(self, event, extension):
+
+        ssid = event.get_data().pop()
+        os.system(f"bash -c '/usr/bin/nmcli dev wifi connect {ssid}'")
+
+        return RenderResultListAction(
+            [
+                ExtensionResultItem(
+                    icon="images/icon.png",
+                    name=f"Connected to {ssid}",
+                    on_enter=HideWindowAction(),
+                )
+            ]
+        )
 
 
 if __name__ == "__main__":
